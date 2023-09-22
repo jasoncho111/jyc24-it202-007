1b224b5a06d1b7c46e08fe29987eb649c06a9917
jasoncho111 <jyc24@njit.edu>
2023-09-22 15:28:48 -0400

    problem 2 complete

--- public_html/M2/problem2.php
+++ public_html/M2/problem2.php
@@ -8,8 +8,14 @@ function getTotal($arr) {
     $total = 0.00;
     //note: use the $arr variable, don't directly touch $a1-$a4
     //TODO do adding here
+    foreach ($arr as $num) {
+        $total += $num;
+    }
     //TODO do rounding stuff here (round to two decimals i.e., 0.10, 0.01, 0.00)
-    echo "The total is " . var_export($total, true);
+    $total *= 100;
+    $total = (int) $total;
+    $total /= 100;
+    echo "The total is " . var_export($total, true) . (((int)($total * 100) % 10 == 0) ? "0" : "");
 }
 echo "Problem 2: Adding Floats<br>";
 ?>
