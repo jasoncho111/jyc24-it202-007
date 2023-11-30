382112334eef3b2fc884dbd220b2ca2ccdd21a0c
jasoncho111 <jyc24@njit.edu>
2023-11-30 18:32:03 -0500

    order by query added

--- public_html/Project/list_countries.php
+++ public_html/Project/list_countries.php
@@ -2,31 +2,38 @@
 require(__DIR__ . "/../../partials/nav.php");
 is_logged_in(true);
 
-$query = "SELECT country_name, capital, population, FROM Countries";
+$query = "SELECT country_name, capital, population FROM Countries";
 $sname = "";
 $scap = "";
 $slim = 10;
+$order = "ASC";
 if(isset($_POST["name"]) || isset($_POST["capital"])) {
     $query .= " WHERE ";
-    if(isset($_POST["name"])) {
+    if(isset($_POST["name"]) && !empty($_POST["name"])) {
         $sname = se($_POST, "name", "", false);
         $query .= "country_name LIKE '%$sname%', ";
     }
-    if(isset($_POST["capital"])) {
+    if(isset($_POST["capital"]) && !empty($_POST["capital"])) {
         $scap = se($_POST, "capital", "", false);
-        $query .= "capital LIKE '%$scap%'";
+        $query .= "capital LIKE '%$scap%', ";
     }
 
     $query = substr($query, 0, strlen($query) -2);
+}
+if(isset($_POST["order"])) {
+    $order = se($_POST, "order", "", false);
+    $query .= " ORDER BY country_name $order";
 }
 if(isset($_POST["lim"])) {
     $slim = se($_POST, "lim", "", false);
 }
 $query .= " LIMIT $slim";
 
+flash($query);
 if($slim < 1 || $slim > 100) flash("Limit filter must be between 1 and 100 inclusive", "warning");
 else {
-    ;
+    $db = getDB();
+    $stmt = $db->prepare($query);
 }
 ?>
 
@@ -41,8 +48,8 @@ else {
         <?php render_input(["type" => "search", "name" => "capital", "label" => "Country Capital", "placeholder" => "Capital Filter", "value"=>$scap]); ?>
         <?php render_input(["type" => "number", "name" => "lim", "label" => "Max Results", "placeholder" => "Limit", "value"=>$slim, "rules" => ["required" => true, "min" => 1, "max" => 100]]) ?>;
         <p style="margin-left: 7px">Order by:</p>
-        <?php render_input(["type" => "radio", "name" => "order", "label" => "Ascending", "value" => "asc", "rules" => ["checked" => true]]); ?>
-        <?php render_input(["type" => "radio", "name" => "order", "label" => "Descending", "value" => "desc"]); ?>
+        <?php render_input(["type" => "radio", "name" => "order", "label" => "Ascending", "value" => "ASC", "rules" => ($order == "ASC" ? ["checked" => true] : [])]); ?>
+        <?php render_input(["type" => "radio", "name" => "order", "label" => "Descending", "value" => "DESC", "rules" => ($order == "DESC" ? ["checked" => true] : [])]); ?>
         <?php render_button(["text" => "Search", "type" => "submit", "color" => "primary"]); ?>
     </form>
 </div>
