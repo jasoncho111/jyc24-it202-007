fe4eb7c9eea30726d0c8d80bdb3d680714298e25
jasoncho111 <jyc24@njit.edu>
2023-11-29 21:53:01 -0500

    added functionality to nav and added link to new data creation page

--- partials/nav.php
+++ partials/nav.php
@@ -34,20 +34,21 @@ session_start();
 <nav>
     <ul>
         <?php if (is_logged_in()) : ?>
-            <li><a href="<?php echo get_url('home.php'); ?>">Home</a></li>
-            <li><a href="<?php echo get_url('profile.php'); ?>">Profile</a></li>
+            <li onclick="location.replace('<?php echo get_url('home.php')?>');"><a href="<?php echo get_url('home.php'); ?>">Home</a></li>
+            <li onclick="location.replace('<?php echo get_url('profile.php')?>');"><a href="<?php echo get_url('profile.php'); ?>">Profile</a></li>
         <?php endif; ?>
         <?php if (!is_logged_in()) : ?>
-            <li><a href="<?php echo get_url('login.php'); ?>">Login</a></li>
-            <li><a href="<?php echo get_url('register.php'); ?>">Register</a></li>
+            <li onclick="location.replace('<?php echo get_url('login.php')?>');"><a href="<?php echo get_url('login.php'); ?>">Login</a></li>
+            <li onclick="location.replace('<?php echo get_url('register.php')?>');"><a href="<?php echo get_url('register.php'); ?>">Register</a></li>
         <?php endif; ?>
         <?php if (has_role("Admin")) : ?>
-            <li><a href="<?php echo get_url('admin/create_role.php'); ?>">Create Role</a></li>
-            <li><a href="<?php echo get_url('admin/list_roles.php'); ?>">List Roles</a></li>
-            <li><a href="<?php echo get_url('admin/assign_roles.php'); ?>">Assign Roles</a></li>
+            <li onclick="location.replace('<?php echo get_url('admin/create_role.php')?>');"><a href="<?php echo get_url('admin/create_role.php'); ?>">Create Role</a></li>
+            <li onclick="location.replace('<?php echo get_url('admin/list_roles.php')?>');"><a href="<?php echo get_url('admin/list_roles.php'); ?>">List Roles</a></li>
+            <li onclick="location.replace('<?php echo get_url('admin/assign_roles.php')?>');"><a href="<?php echo get_url('admin/assign_roles.php'); ?>">Assign Roles</a></li>
+            <li onclick="location.replace('<?php echo get_url('admin/create_edit_countries.php')?>');"><a href="<?php echo get_url('admin/create_edit_countries.php'); ?>">Create Countries</a></li>
         <?php endif; ?>
         <?php if (is_logged_in()) : ?>
-            <li><a href="<?php echo get_url('logout.php'); ?>">Logout</a></li>
+            <li onclick="location.replace('<?php echo get_url('logout.php')?>');"><a href="<?php echo get_url('logout.php'); ?>">Logout</a></li>
         <?php endif; ?>
     </ul>
 </nav>
